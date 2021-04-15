14bf04fd026b548f75f4a34f9ac00e258eacc032
Chris Analikwu <analikwuchristopher@gmail.com>
2021-04-15 06:08:34 +0200

    Tyson agency web app

---
+++ Tyson-agencyapp/js/script.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.toggle')
+const navigation = document.querySelector('.navigation')
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('active')
+    navigation.classList.toggle('active')
+})
+
